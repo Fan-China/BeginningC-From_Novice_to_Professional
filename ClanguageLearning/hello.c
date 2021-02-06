58db2aba217b17027c3329d80aad693683e0513e
Fan <1571672031@qq.com>
2021-02-06 18:34:12 +0800

    Learned Chapter 4.12 Program 4.13 The hardest coding program ever for me, failed.

--- ClanguageLearning/hello.c
+++ ClanguageLearning/hello.c
@@ -11,7 +11,7 @@
 #define PI 3.14159f                            //Definition of the symbol PI
 #include <limits.h>                            //For limits on integer types
 #include <float.h>                             //For limits on floating-point types
-#include <stdbool.h>
+#include <stdbool.h>                           // For bool, true adn false
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>                            // For rand() and srand()
@@ -19,6 +19,109 @@
 int main2_1(void);
 int main(void)
 {
+    // Program 4.13 Simple Simon                        // Feb06, 2021. 未完成，待以后完成, 还不能执行预期功能
+    char another_game = 'Y';
+    const unsigned int DELAY = 1;
+    bool correct = true;
+    unsigned int tries = 0;
+    unsigned digits = 0;
+    time_t seed = 0;
+    unsigned int number = 0;
+    time_t wait_start = 0;
+    clock_t start_time = 0;
+    unsigned int score = 0;
+    unsigned int total_digits = 0;
+    unsigned int game_time = 0;
+    printf("To play simon, watch the screen for a sequence of digits.\n"
+           "Watch carefully, as the digits are only displayed for %u second%s\n", DELAY, DELAY > 1 ? "s!" : "!" );
+    printf("The computer will remove them, and then prompt you to enter the same sequence.\n"
+           "When you do, you must put spaces between the digits.\n");
+    printf("Good luck! Press Enter to play!\n");
+    scanf("%c", &another_game);
+    do
+    {
+        // Initialize game
+        correct = true;                // Indicates correct sequence entered
+        tries = 0;                     // Initialize count of successful tries
+        digits = 2;                    // Initial length of digits sequence
+        start_time = clock();          // Record time at start of game
+        
+    // Inner loop continues as long as sequnceses are entered correctly
+    while(correct)
+    {
+        ++tries;                        // A new attempt
+        wait_start = clock();           // Record start time for sequence
+        
+        // Generate a sequence of digits and display them
+        srand(time(&seed));             // Initialize the ramdom sequence
+        for(unsigned int i = 1; i <= digits; ++i)
+        {
+            printf("%u", rand() % 10);  // Output a random digit
+        }
+        for(; clock() - wait_start < DELAY * CLOCKS_PER_SEC; )     // Wait delay seconds
+        {}
+        // Now overwrite the digit sequence
+        printf("\r");                   // Go to beginning of the line
+        for(unsigned int i = 1; i <= digits; ++i)
+        {
+            printf("  ");               // Output 2 spaces
+        }
+        
+        if(tries == 1)                  // Only output message for 1st try
+        {
+            printf("\nNow you enter the sequence - don't forget the spaces\n");
+        }
+        
+        else                            // Back to the beginning of the line
+        {
+            printf("\r");
+        }
+        
+        srand(seed);                    // Reinitialize the random sequence
+        for (unsigned int i = 1; i <= digits; ++i)
+        // Read the input sequence & check against the original
+        {
+            scanf("%u", &number);       // Read a digit
+            if (number != rand() % 10)  // Compare with generated digits
+            {
+                correct = false;        // Incorrect entry
+                break;                  // No need for check further……
+            }
+        }
+        
+        // On every 3rd successful try, increase the sequence length
+        if(correct && ((tries % 3) == 0))
+        {
+            ++digits;
+        }
+        
+        printf("%s\n", correct ? "Correct!" : "Wrong!");
+    }
+        
+    // Calculate and output the game score
+    score = 10 * (digits - (tries % 3) == 1);       // Points for sequence length
+    total_digits = digits * (((tries % 3) == 0) ? 3 : tries % 3);
+    if(digits > 2)
+    {
+        total_digits += 3 * ((digits - 1) * (digits - 2) / 2 - 1);
+    }
+        
+    game_time = (clock() - start_time) / CLOCKS_PER_SEC - tries * DELAY;
+    
+    if(total_digits > game_time)
+    {
+        score += 10 * (game_time - total_digits);   //Add points for speed
+    }
+    printf("\n\nGame time was %u seconds. Your score is %u", game_time, score);
+    
+    fflush(stdin);                                  // Clear the input buffer
+    
+    // Check if new game required
+    printf("Do you want to paly again?(y/n)\n ");
+    scanf("%c", &another_game);
+        
+    }while (another_game == 'Y');
+    /*
     // Program 4.12 Reverting the digits
     unsigned int number = 0;
     unsigned int rebmun = 0;
@@ -27,11 +130,11 @@ int main(void)
     scanf("%u", &number);
     temp = number;
     do {
-        rebmun = rebmun * 10 + temp % 10;
+        rebmun = rebmun * 10 + temp % 10;           // 这种解法会更简洁，但同时更不易读
         temp = temp / 10;
     } while (temp);
     printf("The reversed number is %u on %u.\n", rebmun, number);
-    /*
+    
     if( number > 9 )
     {
         do
